e3ec7a0a9054743305206e7a73cb5fc881405268
SAMI-THAKUR <2022.sami.thakur@ves.ac.in>
2023-09-09 17:28:35 +0530

    final

--- script.js
+++ script.js
@@ -18,11 +18,14 @@ const typeWriter = () => {
     setTimeout(() => typeWriter(), 600)
 }
 typeWriter();
+
+
 // -------------------------------DARK_LIGHT TOGGLE------------------------------------//
 var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
-// Change the icons inside the button based on previous settings
+
+
 if (localStorage.getItem('color-theme') === 'dark') {
     themeToggleLightIcon.classList.remove('hidden');
 } else {
@@ -31,33 +34,37 @@ if (localStorage.getItem('color-theme') === 'dark') {
 
 var themeToggleBtn = document.getElementById('theme-toggle');
 
+if (localStorage.getItem('color-theme')) {
+    if (localStorage.getItem('color-theme') === 'dark') {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+    }
+}
+
 themeToggleBtn.addEventListener('click', function() {
 
+    // Change the icons inside the button based on previous settings
+
     // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
     themeToggleLightIcon.classList.toggle('hidden');
+    themeToggleDarkIcon.classList.toggle('hidden');
+   
 
     // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        }
+ 
 
     // if NOT set via local storage previously
-    } else {
+
         if (document.documentElement.classList.contains('dark')) {
             document.documentElement.classList.remove('dark');
             localStorage.setItem('color-theme', 'light');
         } else {
             document.documentElement.classList.add('dark');
             localStorage.setItem('color-theme', 'dark');
-        }
-    }
-    
+        }  
 });
 
 
